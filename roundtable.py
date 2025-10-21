0f19591c2e7b43d42adcf82e0e6af98055877d52
samldr <sleader@ualberta.ca>
2025-10-21 16:41:18 -0600

    feat: add populate command
    Added the populate command to add tasks to the backlog for high-level
    scheduling. Also added the -n flag to the update command to add a note
    to issues.
    
    Signed-off-by: samldr <sleader@ualberta.ca>

--- roundtable.py
+++ roundtable.py
@@ -8,6 +8,7 @@ from rich import print
 from rich.table import Table
 from rich.console import Console
 from rich.prompt import Prompt, Confirm, IntPrompt
+from typing_extensions import Annotated
 
 app = typer.Typer()
 console = Console()
@@ -49,7 +50,7 @@ def main():
 
 
 @app.command()
-def update():
+def update(notes: Annotated[bool, typer.Option("-n","--note", help="Add notes while updating issues")] = False):
     """
     Update assigned Redmine issues
     """
@@ -123,6 +124,15 @@ def update():
                         due_date= tuesday() + timedelta(6)
                     )
                     print(f'New due date: [bold white]{tuesday() + timedelta(6)}')
+
+            if notes:
+                note = Prompt.ask("Note")
+                if note.strip():
+                    try:
+                        redmine.issue.update(issue.id, notes=note)
+                        print(f'Note added: [bold white]{note}')
+                    except Exception as e:
+                        print(f'[red]Error adding note: {e}')
             print(f'Link to issue: {REDMINE_URL}/issues/{issue.id}\n')    
 
 
@@ -206,5 +216,92 @@ def new():
         if not loop:
             break  
 
+@app.command()
+def populate():
+    """
+    Add tasks to the backlog
+    """
+    while True:
+        new_issue = {}
+        print("[bold italic]Create New Backlog Issue")
+
+        # title and description
+        new_issue['subject'] = Prompt.ask("Issue Subject")
+        
+        what = Prompt.ask("What is the task?")
+        why = Prompt.ask("Why are we doing this task?")
+
+        # dates
+        while True:
+            try:
+                start = datetime.strptime(Prompt.ask("Start date ([bold]YYYY-MM-DD[/bold])"), '%Y-%m-%d').date()
+                deadline = datetime.strptime(Prompt.ask("Deadline ([bold]YYYY-MM-DD[/bold])"), '%Y-%m-%d').date()
+            except ValueError as e:
+                print(f"[red]Invalid date format: {e}")
+                continue
+            break
+
+        new_issue['due_date'] = deadline
+        new_issue['start_date'] = start
+
+        # parent task (topic)
+        topics = redmine.issue.filter(project_id=project.id, tracker_id=8)
+
+        table = Table(show_footer=False, title_justify="left")
+        table.add_column("Index")
+        table.add_column("Topic")
+        for index, issue in enumerate(topics):
+            table.add_row(str(index), issue.subject)
+        console.print(table)
+        topic_index = Prompt.ask("Select Parent Task", choices=[str(i) for i in range(len(topics))])
+
+        new_issue['parent_issue_id'] = topics[int(topic_index)].id
+
+        # category
+        categories = project.issue_categories
+        table = Table(show_footer=False, title_justify="left")
+        table.add_column("Index")
+        table.add_column("Category")
+        for index, category in enumerate(categories):
+            table.add_row(str(index), category.name)
+        console.print(table)
+        category_index = Prompt.ask("Select Category", choices=[str(i) for i in range(len(categories))])
+
+        new_issue['category_id'] = categories[int(category_index)].id
+
+        # misc
+        new_issue['description'] = f"Who: \nWhat: {what}\nWhy: {why}"        
+        new_issue['project_id'] = project.id
+        new_issue['tracker_id'] = 7  # Task
+        new_issue['status_id'] = 1  # In Progress
+        new_issue['priority_id'] = 2  # Normal
+        new_issue['done_ratio'] = 0  # 0%
+
+        # confirmation
+        print("[bold]Issue Summary:", new_issue)
+        confirm = Confirm.ask("Create this issue?")
+        if confirm:
+            try:
+                issue = redmine.issue.create(**new_issue)
+                print(f"Issue created: {REDMINE_URL}/issues/{issue.id}\n")
+            except Exception as e:
+                print(f"[red]Error creating issue: {e}")
+
+        prev_issue = redmine.issue.get(issue.id - 1)
+        if prev_issue.project.id == project.id:
+            blocker = Confirm.ask(f'Is this issue a blocked by the previous issue {prev_issue.id}: {prev_issue.subject}?')
+            if blocker:
+                try:
+                    redmine.issue_relation.create(issue_id=prev_issue.id, issue_to_id=issue.id, relation_type='blocks')
+                    print(f'Added blocker: {REDMINE_URL}/issues/{prev_issue.id} blocks {REDMINE_URL}/issues/{issue.id}')
+                except Exception as e:
+                    print(f'[red]Error adding blocker: {e}')
+        
+
+        loop = Confirm.ask("Add another issue?")
+        if not loop:
+            break  
+
 if __name__ == "__main__":
     app()
+    
